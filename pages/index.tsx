8a124c834c1b33de678b551b4400a4db018c487f
Igor Lourenço <theigorlourenco@gmail.com>
2020-09-29 18:44:12 -0300

    Connection to Mongo DB cloud

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,56 +1,60 @@
-import {useState} from 'react'
-import { Flex, Button, Image, Text } from '@chakra-ui/core'
+import {FormEvent, useState} from 'react'
+import {Flex, Button, Image, Text} from '@chakra-ui/core'
 import Input from '../components/Input'
+import axios from 'axios'
 
 export default function Home() {
-  const [email, setEmail] = useState();
+    const [email, setEmail] = useState();
 
-  function handleSignUpToNewsletter(){}
+    function handleSignUpToNewsletter(event: FormEvent) {
+        event.preventDefault()
+        axios.post('/api/subscribe', {email})
+    }
 
 
-  return (
-      <Flex
-          as="main"
-          height="100vh"
-          justifyContent="center"
-          alignItems="center"
-      >
+    return (
         <Flex
-            as="form"
-            onSubmit={handleSignUpToNewsletter}
-            backgroundColor="gray.700"
-            borderRadius="md"
-            flexDir="column"
-            alignItems="stretch"
-            padding={8}
-            marginTop={4}
-            width="100%"
-            maxW="400px"
+            as="main"
+            height="100vh"
+            justifyContent="center"
+            alignItems="center"
         >
-          {/*<Image marginBottom={8} src="/rocketseat.svg" alt="Rocketseat" />*/}
+            <Flex
+                as="form"
+                onSubmit={handleSignUpToNewsletter}
+                backgroundColor="gray.700"
+                borderRadius="md"
+                flexDir="column"
+                alignItems="stretch"
+                padding={8}
+                marginTop={4}
+                width="100%"
+                maxW="400px"
+            >
+                {/*<Image marginBottom={8} src="/rocketseat.svg" alt="Rocketseat" />*/}
 
-          <Text textAlign="center" fontSize="sm" color="gray.400" marginBottom={2}>
-            Assine a newsletter e receba os melhores conteúdos!
-          </Text>
+                <Text textAlign="center" fontSize="sm" color="gray.400" marginBottom={2}>
+                    Assine a newsletter e receba os melhores conteúdos!
+                </Text>
 
-          <Input
-              placeholder="Seu e-mail"
-              marginTop={2}
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-          />
+                <Input
+                    placeholder="Seu e-mail"
+                    marginTop={2}
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                />
 
-          <Button
-              type="submit"
-              backgroundColor="blue.500"
-              height="50px"
-              borderRadius="sm"
-              marginTop={6}
-              _hover={{ backgroundColor: 'blue.600' }}
-          >
-            INSCREVER
-          </Button>
+                <Button
+                    type="submit"
+                    backgroundColor="blue.500"
+                    height="50px"
+                    borderRadius="sm"
+                    marginTop={6}
+                    _hover={{backgroundColor: 'blue.600'}}
+                >
+                    INSCREVER
+                </Button>
+            </Flex>
         </Flex>
-      </Flex>
-  )
+    )
 }
