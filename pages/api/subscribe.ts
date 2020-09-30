6b7b2a66a34dbb7b05320f38ceee068f7218432a
Igor Lourenço <theigorlourenco@gmail.com>
2020-09-30 13:10:42 -0300

    Send email with Sendgrid

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -1,6 +1,7 @@
 import {NowRequest, NowResponse} from '@vercel/node'
 import {MongoClient, Db} from 'mongodb'
 import url from 'url'
+import MailService from "@sendgrid/mail"
 
 let cachedDb: Db = null
 
@@ -27,6 +28,16 @@ async function connectToDatabase(uri: string) {
 export default async (request: NowRequest, response: NowResponse) => {
     const {email} = request.body;
 
+    MailService.setApiKey(process.env.SENDGRID_API_KEY);
+
+    const message = {
+        to: email,
+        from: 'theigorlourenco@gmail.com',
+        subject: 'Inscrição na minha sewsletter',
+        text: 'Você se inscreveu na newsletter do Igor Lourenço',
+        html: '<br><strong>essa mensagem é um teste</strong>'
+    }
+
     const db = await connectToDatabase(process.env.MONGODB_URI);
     const collection = db.collection('subscribers');
 
@@ -34,6 +45,18 @@ export default async (request: NowRequest, response: NowResponse) => {
         email,
         subscribedAt: new Date()
     });
+
+    await (async () => {
+        try {
+            await MailService.send(message)
+        } catch (error) {
+            console.error(error);
+
+            if (error.response) {
+                console.error(error.response.body)
+            }
+        }
+    })();
 
     return response.status(201).json({ok: true});
 }
