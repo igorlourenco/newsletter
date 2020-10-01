dfba872bcd457022d09c519aafeecc778eb277e4
Igor Lourenço <theigorlourenco@gmail.com>
2020-10-01 13:44:58 -0300

    Finish subscribe

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -33,8 +33,6 @@ export default async (request: NowRequest, response: NowResponse) => {
     const db = await connectToDatabase(process.env.MONGODB_URI);
     const collection = db.collection('subscribers');
 
-    console.log(db.collection('subscribers').find({}));
-
     await collection.insertOne({
         email,
         subscribedAt: new Date()
